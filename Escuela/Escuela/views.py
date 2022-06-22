b3d4f588e308b2a29fee2329bd8f777f7681ed43
FrancoCifuentesGizzi <fcgizzi@gmail.com>
2022-06-22 17:20:28 -0400

    luchito gei

---
+++ Escuela/Escuela/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.template import loader
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+
+
+# Create your views here.
+
+def home(request):
+    # variable de session num_visitas.
+    num_visitas = request.session.get('num_visitas', 0)
+    request.session['num_visitas'] = num_visitas + 1
+    
+    # default template.
+    template = loader.get_template('home.html')
+    context = {
+        'settings': settings,
+        'dataarticulos': None,
+        'num_visitas': num_visitas,
+    }
+
+    if (request.user.is_authenticated):
+        # es superusuario?
+        if (request.user.is_superuser):
+            return HttpResponseRedirect('admin/')
+
+
+
+
+    return HttpResponse(template.render(context, request))
